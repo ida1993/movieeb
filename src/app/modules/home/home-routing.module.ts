3cd1fd2795abe4a8e7f9d7dc45c7cbb2a1f1d953
ida bådsvik <ida.e.baadsvik@gmail.com>
2019-03-20 10:35:22 +0100

    ADD: imports, paths, and exports

--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -1,17 +1,32 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import {Routes} from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from './pages/home/home.component';
+import {TopRatedMoviesComponent} from './pages/top-rated-movies/top-rated-movies.component';
+import {TopRatedTvshowsComponent} from './pages/top-rated-tvshows/top-rated-tvshows.component';
+import {PopularPeopleComponent} from './pages/popular-people/popular-people.component';
 
 const routes: Routes = [
   {
-
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'Top-rated-movies',
+    component: TopRatedMoviesComponent
+  },
+  {
+    path: 'Top-rated-tv-shows',
+    component: TopRatedTvshowsComponent
+  },
+  {
+    path: 'Popular-people',
+    component: PopularPeopleComponent
   }
+
 ];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
 })
 export class HomeRoutingModule { }
