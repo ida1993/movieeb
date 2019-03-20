904231a3986a8df2ef3f5dfb6cb0735ac1144153
ida bådsvik <ida.e.baadsvik@gmail.com>
2019-03-20 10:33:14 +0100

    ADD:import topratedmovies, popularpeople, topratedtvshows

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import {HomeRoutingModule} from './home-routing.module';
 import {SharedModule} from '../../shared/shared.module';
+import { TopRatedMoviesComponent } from './pages/top-rated-movies/top-rated-movies.component';
+import { PopularPeopleComponent } from './pages/popular-people/popular-people.component';
+import { TopRatedTvshowsComponent } from './pages/top-rated-tvshows/top-rated-tvshows.component';
 
 @NgModule({
   declarations: [
+    TopRatedMoviesComponent,
+    PopularPeopleComponent,
+    TopRatedTvshowsComponent
   ],
   imports: [
-    CommonModule,
     HomeRoutingModule,
     SharedModule
-
   ]
 })
 export class HomeModule { }
