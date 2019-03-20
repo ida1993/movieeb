bc6269bc00b364da28e9d3008187a2abcae36dd8
ida bådsvik <ida.e.baadsvik@gmail.com>
2019-03-20 10:34:44 +0100

    ADD: imports, declarations and exports

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {TopMenuComponent} from './top-menu/top-menu.component';
+import {SearchComponent} from './search/search.component';
+import {RouterModule} from '@angular/router';
+import {UpcomingMoviesComponent} from '../modules/home/pages/upcoming-movies/upcoming-movies.component';
+import {HomeComponent} from '../modules/home/pages/home/home.component';
+import {MovieComponent} from './movie/movie.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent,
+    MovieComponent,
+    SearchComponent,
+    TopMenuComponent,
+    UpcomingMoviesComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
+  ],
+  exports: [
+    CommonModule,
+    HomeComponent,
+    MovieComponent,
+    SearchComponent,
+    TopMenuComponent,
+    UpcomingMoviesComponent
   ]
 })
 export class SharedModule { }
