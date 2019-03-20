e299269d525e71c4d6b3601e894e75eeda9d361d
ida bådsvik <ida.e.baadsvik@gmail.com>
2019-03-20 08:37:08 +0100

    ADD:

---
+++ src/app/modules/home/home-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {Routes} from '@angular/router';
+
+const routes: Routes = [
+  {
+
+  }
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class HomeRoutingModule { }
