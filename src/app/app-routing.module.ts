d8b66be495431161b05f53c8d548c6013f63d15f
ida bådsvik <ida.e.baadsvik@gmail.com>
2019-03-21 13:55:30 +0100

    ADD:imports, paths and load children

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import {HomeModule} from './modules/home/home.module';
 import {RouterModule, Routes} from '@angular/router';
 import {TvShowsModule} from './modules/tv-shows/tv-shows.module';
+import {MovieModule} from './modules/movie/movie.module';
+import {PeopleModule} from './modules/people/people.module';
 
 const appRoutes: Routes = [
   {
@@ -11,6 +13,14 @@ const appRoutes: Routes = [
   {
     path: 'tv-shows',
     loadChildren: () => TvShowsModule
+  },
+  {
+    path: 'movies',
+    loadChildren: () => MovieModule
+  },
+  {
+    path: 'people',
+    loadChildren: () => PeopleModule
   }
 ];
 
