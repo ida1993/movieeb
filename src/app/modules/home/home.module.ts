dba51e66faf280ecd2fc237d1148cedab378792e
ida bådsvik <ida.e.baadsvik@gmail.com>
2019-03-21 13:42:27 +0100

    REMOVE: import TopRatedMoviesComponent and PopularPeopleComponent

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,13 +1,11 @@
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
 import {HomeRoutingModule} from './home-routing.module';
 import {SharedModule} from '../../shared/shared.module';
-import { TopRatedMoviesComponent } from './pages/top-rated-movies/top-rated-movies.component';
-import { PopularPeopleComponent } from './pages/popular-people/popular-people.component';
+import {HomeComponent} from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    TopRatedMoviesComponent,
-    PopularPeopleComponent
+    HomeComponent
   ],
   imports: [
     HomeRoutingModule,
