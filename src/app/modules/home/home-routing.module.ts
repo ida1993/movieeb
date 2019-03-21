41e8e7ef71b8c13a641fa14cba7990f880faec8b
ida bådsvik <ida.e.baadsvik@gmail.com>
2019-03-21 13:43:32 +0100

    REMOVE: import TopRatedMoviesComponent and PopularPeopleComponent, and path movies/to-rated and people/popular

--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -1,21 +1,11 @@
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from './pages/home/home.component';
-import {TopRatedMoviesComponent} from './pages/top-rated-movies/top-rated-movies.component';
-import {PopularPeopleComponent} from './pages/popular-people/popular-people.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent
-  },
-  {
-    path: 'movies/top-rated',
-    component: TopRatedMoviesComponent
-  },
-  {
-    path: 'people/popular',
-    component: PopularPeopleComponent
   }
 
 ];
