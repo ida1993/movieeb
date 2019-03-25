7b0088bab258ea77ad676a5d230000431cf54d42
ida bådsvik <ida.e.baadsvik@gmail.com>
2019-03-25 10:11:27 +0100

    ADD: import peopleInfoComponent and PeopleResolver. Add new path.

--- src/app/modules/people/people-routing.module.ts
+++ src/app/modules/people/people-routing.module.ts
@@ -1,12 +1,22 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {PopularPeopleComponent} from './pages/popular-people/popular-people.component';
+import {PeopleInfoComponent} from './pages/people-info/people-info.component';
+import {PeopleResolver} from '../../core/resolvers/PeopleResolver';
 
 
 const routes: Routes = [
   {
     path: 'popular',
     component: PopularPeopleComponent
+  },
+  {
+    path: ':id',
+    component: PeopleInfoComponent,
+    resolve: {
+      people: PeopleResolver
+    }
+
   }
 ];
 
