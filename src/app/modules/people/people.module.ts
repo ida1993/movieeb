60a08d8e8c418f43de350642ab10f7ff96b4da02
ida bådsvik <ida.e.baadsvik@gmail.com>
2019-03-25 10:11:50 +0100

    ADD: import PeopleInfoComponent and declaration

--- src/app/modules/people/people.module.ts
+++ src/app/modules/people/people.module.ts
@@ -2,10 +2,12 @@ import {NgModule} from '@angular/core';
 import {SharedModule} from '../../shared/shared.module';
 import {PeopleRoutingModule} from './people-routing.module';
 import {PopularPeopleComponent} from './pages/popular-people/popular-people.component';
+import { PeopleInfoComponent } from './pages/people-info/people-info.component';
 
 @NgModule({
   declarations: [
-    PopularPeopleComponent
+    PopularPeopleComponent,
+    PeopleInfoComponent
   ],
   imports: [
     PeopleRoutingModule,
