2cb41e9d3b0e0ef49771da433006feea9e5437b8
ida bådsvik <ida.e.baadsvik@gmail.com>
2019-03-25 10:06:48 +0100

    ADD: tv-shows.component

--- src/app/shared/tv-shows/tv-shows.component.ts
+++ src/app/shared/tv-shows/tv-shows.component.ts
@@ -1,15 +1,16 @@
-// import {Component, Input, OnInit} from '@angular/core';
-//
-// @Component({
-//   selector: 'app-tv-shows',
-//   templateUrl: './tv-shows.component.html',
-//   styleUrls: ['./tv-shows.component.scss']
-// })
-// export class TvShowsComponent implements OnInit {
-//   @Input() tv;
-//   constructor() { }
-//
-//   ngOnInit() {
-//   }
-//
-// }
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-tv-shows',
+  templateUrl: './tv-shows.component.html',
+  styleUrls: ['./tv-shows.component.scss']
+})
+
+export class TvShowsComponent implements OnInit {
+  @Input() tv;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
