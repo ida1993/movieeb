d8f3378c9d62cb890fb050a4d633e2aeb636277e
ida bådsvik <ida.e.baadsvik@gmail.com>
2019-03-27 11:01:04 +0100

    ADD: import TvShowsInfoComponent

--- src/app/modules/tv-shows/tv-shows.module.ts
+++ src/app/modules/tv-shows/tv-shows.module.ts
@@ -2,10 +2,12 @@ import {NgModule} from '@angular/core';
 import {TopRatedTvshowsComponent} from './pages/top-rated-tvshows/top-rated-tvshows.component';
 import {SharedModule} from '../../shared/shared.module';
 import {TvShowsRoutingModule} from './tv-shows-routing.module';
+import { TvShowsInfoComponent } from './pages/tv-shows-info/tv-shows-info.component';
 
 @NgModule({
   declarations: [
-    TopRatedTvshowsComponent
+    TopRatedTvshowsComponent,
+    TvShowsInfoComponent
   ],
   imports: [
     TvShowsRoutingModule,
